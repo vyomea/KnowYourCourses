058e4c721648a0b3e361b53d5ad16dab53eaa848
ashthegod <68326511+ashthegod@users.noreply.github.com>
2021-01-16 22:31:57 -0700

    combining data stuff

--- api/views.py
+++ api/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Blueprint, jsonify
 
 main = Blueprint("main", __name__)
@@ -7,12 +6,4 @@ main = Blueprint("main", __name__)
 def getRating():
     rating = 0
 
-=======
-from flask import Blueprint, jsonify
-
-@main.route('/rating')
-def getRating():
-    rating = 0
-
->>>>>>> 377ce21e0033dc241b0bfdbc5463e4de6a932b13
     return jsonify({'rating':rating})
--- classify_class.py
+++ classify_class.py
@@ -7,7 +7,8 @@ import pandas as pd
 import spacy
 from spacy.util import minibatch, compounding
 import numpy
-
+from bs4 import BeautifulSoup
+import requests
 def getDetails():
 
     # Acessing the reddit api
@@ -99,9 +100,47 @@ def predict(input_data: str = "no"):
     # )
 
     return prediction, score
+def getdifficulty(tid):
+    URL = 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid='+ str(tid)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    elems = soup.find(id = 'root')
+    res = elems.find_all('div',class_='FeedbackItem__FeedbackNumber-uof32n-1 kkESWs')
+    return str(res[1].text)
+def getpercentage(tid):
+    URL = 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid='+ str(tid)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    elems = soup.find(id = 'root')
+    res = elems.find_all('div',class_='FeedbackItem__FeedbackNumber-uof32n-1 kkESWs')
+    #print(res[0].text)
+    return str(res[0].text)
 
+#get all data for a prof, returns a tuple (rating,total_ratings,difficulty,percentageRetake)
+def find_prof(name):
+    f = open("data.txt","r")
+    data = f.readlines()
+    tid = ""
+    rating = ""
+    total_ratings = ""
+    difficulty = ""
+    percentageRetake = ""
+    for i in range(0,len(data)):
+        if(data[i].strip() == "professor name:"+name):
+            tid = data[i+1].split(':')[1].strip()
+            rating = data[i+2].split(':')[1].strip()
+            total_ratings = data[i+3].split(':')[1].strip()
+            difficulty = getdifficulty(tid)
+            percentageRetake = getpercentage(tid)
+            percentageRetake = percentageRetake[0:2]
+            print(tid)
+            print(rating)
+            print(total_ratings)
+            print(difficulty)
+            print(percentageRetake)
+    return (rating,total_ratings,difficulty,percentageRetake)
 
-def getCourseDifficulty():
+def getCourseDifficulty(name,course):
 
     return True
 
